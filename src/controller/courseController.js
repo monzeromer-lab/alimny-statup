e24601c8605e6cfc1949dd397f0fdaf0833f3cc3
Monzer Abdullaziz <nezonru87@yahoo.com>
2022-01-24 15:50:12 +0200

    added: intro upload controller

--- src/controller/courseController.js
+++ src/controller/courseController.js
@@ -2,8 +2,30 @@ const {
     createCourse,
     createCoupon,
     studentWill_learn,
-    course_req
-} = require("../service/courseService.db")
+    course_req,
+    postIntro,
+    getCourseIntro
+} = require("../service/courseService.db"),
+    path = require("path"),
+    uploadPath = path.join("./public/videos")
+
+function renameFile(rename) {
+    return `VID-${Date.now()}${path.extname(rename)}`
+}
+
+function getFilePath(file) {
+    return `/public/videos/${renameFile(file)}`
+}
+
+function fileExtname(file) {
+    return path.extname(file)
+}
+
+function haveIntro(current) {
+    let test = /public/
+
+    return test.test(current)
+}
 
 module.exports.newCourse_controller = async (req, res, next) => {
     let {
@@ -16,7 +38,7 @@ module.exports.newCourse_controller = async (req, res, next) => {
         id
     } = req.user
 
-    await createCourse(req.body, id).then( async (success) => {
+    await createCourse(req.body, id).then(async (success) => {
         if (coupon.code)
             await createCoupon({
                 course_id: success.insertId,
@@ -37,4 +59,73 @@ module.exports.newCourse_controller = async (req, res, next) => {
         })
     })
 
+}
+
+
+module.exports.courseIntro_controller = async (req, res, next) => {
+
+    let {
+        course_intro
+    } = req.files,
+        filePath = uploadPath + `/${renameFile(course_intro.name)}`,
+        exts = [".mp4", ".mov", ".mkv", ".avi"]
+
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        res.status(400).json({
+            error: {
+                state: true,
+                errorMessage: "No files were uploaded",
+                errorCode: 400
+            },
+            message: "no files",
+            data: []
+        })
+    } else if (!exts.includes(fileExtname(course_intro.name))) {
+        res.status(400).json({
+            error: {
+                state: true,
+                errorMessage: "the only supported types is .mp4 .mov .mkv or .avi",
+                errorCode: 400
+            },
+            message: "not supported file",
+            data: []
+        })
+
+    } else {
+
+        await getCourseIntro(req.params.courseId).then((success) => {
+            if (haveIntro(success[0].intro)) {
+                res.status(400).json({
+                    error: {
+                        state: true,
+                        errorMessage: "this endpoint only post for the first time",
+                        errorCode: 400
+                    },
+                    message: "update use put method",
+                    data: []
+                })
+            } else {
+                course_intro.mv(filePath, async (err) => {
+                    if (err) throw new Error("err")
+                    await postIntro(getFilePath(course_intro.name), req.params.courseId).then((success) => {
+                        res.status(200).json({
+                            error: false,
+                            message: "success",
+                            data: {
+                                fileName: renameFile(course_intro.name),
+                                fileMimeType: course_intro.mimetype,
+                                fileSize: course_intro.size,
+                                isOverSize: course_intro.truncated,
+                                filePublicUrl: getFilePath(course_intro.name)
+                            }
+                        })
+                    })
+
+                })
+            }
+
+        })
+
+    }
 }
--- src/router/course.router.js
+++ src/router/course.router.js
@@ -7,16 +7,12 @@ const express = require("express"),
         create_bodyValidition
     } = require('../validition/courseValiditaion'),
     {
-        newCourse_controller
+        newCourse_controller,
+        courseIntro_controller
     } = require("../controller/courseController"),
     path = require("path"),
-    uploadPath = path.join("./public/videos"),
     tmpPath = path.join("./public/videos/tmp"),
-    fileUpload = require("express-fileupload"),
-    {
-        postIntro,
-        getCourseIntro
-    } = require("../service/courseService.db")
+    fileUpload = require("express-fileupload")
 
 course_router.use(fileUpload({
     limits: {
@@ -27,91 +23,7 @@ course_router.use(fileUpload({
     createParentPath: true
 }))
 
-function renameFile(rename) {
-    return `VID-${Date.now()}${path.extname(rename)}`
-}
-
-function getFilePath(file) {
-    return `/public/videos/${renameFile(file)}`
-}
-
-function fileExtname(file) {
-    return path.extname(file)
-}
-
-function haveIntro(current){
-    let test = /public/
-
-    return test.test(current)
-}
-
 course_router.post("/course/new", authenticateToken, create_bodyValidition, newCourse_controller)
-    .post("/course/:courseId/intro", authenticateToken, async (req, res, next) => {
-
-        let {
-            course_intro
-        } = req.files,
-            filePath = uploadPath + `/${renameFile(course_intro.name)}`,
-            exts = [".mp4", ".mov", ".mkv", ".avi"]
-
-
-        if (!req.files || Object.keys(req.files).length === 0) {
-            res.status(400).json({
-                error: {
-                    state: true,
-                    errorMessage: "No files were uploaded",
-                    errorCode: 400
-                },
-                message: "no files",
-                data: []
-            })
-        } else if (!exts.includes(fileExtname(course_intro.name))) {
-            res.status(400).json({
-                error: {
-                    state: true,
-                    errorMessage: "the only supported types is .mp4 .mov .mkv or .avi",
-                    errorCode: 400
-                },
-                message: "not supported file",
-                data: []
-            })
-
-        } else {
-
-            await getCourseIntro(req.params.courseId).then((success) => {
-                if (haveIntro(success[0].intro)) {
-                    res.status(400).json({
-                        error: {
-                            state: true,
-                            errorMessage: "this endpoint only post for the first time",
-                            errorCode: 400
-                        },
-                        message: "update use put method",
-                        data: []
-                    })
-                } else {
-                    course_intro.mv(filePath, async (err) => {
-                        if (err) throw new Error("err")
-                        await postIntro(getFilePath(course_intro.name), req.params.courseId).then((success) => {
-                            res.status(200).json({
-                                error: false,
-                                message: "success",
-                                data: {
-                                    fileName: renameFile(course_intro.name),
-                                    fileMimeType: course_intro.mimetype,
-                                    fileSize: course_intro.size,
-                                    isOverSize: course_intro.truncated,
-                                    filePublicUrl: getFilePath(course_intro.name)
-                                }
-                            })
-                        })
-
-                    })
-                }
-
-            })
-
-        }
-    })
+    .post("/course/:courseId/intro", authenticateToken, courseIntro_controller)
 
 module.exports = course_router
