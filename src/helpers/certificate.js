2854cd89cac506d936a76be176088be91407c75b
Monzer Abdullaziz <nezonru87@yahoo.com>
2022-02-19 21:52:50 +0200

    updated cerificate maker

--- src/helpers/certificate.js
+++ src/helpers/certificate.js
@@ -8,17 +8,17 @@ class Certificate {
         this.Code = code
     }
     doc = new PDFdocument()
-    generate() {
 
+    generate() {
 
         let {
             Title = this.Title,
-                Creator = this.Creator,
-                // CreationDate = new Date().getDate(),
-                Author = "Alimny"
+            Creator = this.Creator,
+            CreationDate = new Date().getFullYear(),
+            Author = "Alimny"
         } = this.doc.info
 
-        this.doc.text("sup", 11, 11).fontSize(188).fillColor("black", 8)
+        this.doc.text("sup how r u?", 11, 11).fontSize(188).fillColor("black", 8)
 
         return this
 
@@ -47,7 +47,7 @@ class Certificate {
 
     save() {
         try {
-            this.doc.pipe(fs.createWriteStream(`./public/files/${this.Code}.pdf`))
+            this.doc.pipe(fs.createWriteStream(`../../public/files/${this.Code}.pdf`))
             this.doc.end()
         } catch (error) {
             console.log(error)
@@ -57,5 +57,5 @@ class Certificate {
 
 }
 
-let sup = new Certificate("backend", "monzer", "r3i")
+let sup = new Certificate("backend", "monzer", "r3i").generate().save()
 module.exports = Certificate
